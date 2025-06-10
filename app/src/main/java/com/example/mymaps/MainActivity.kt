5fd2ab0f2e72d7a3632d5e70d2a092927ef38547
bryandanendra <bryan4icm@gmail.com>
2025-06-10 07:53:16 +0700

    a

---
+++ app/src/main/java/com/example/mymaps/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mymaps
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mymaps.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var fusedLocationClient:FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setup location service
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+}
